3bead5e6a63a2e90d7aa7dc16d2636f3510f72bf
sharkova.ev <sharkova.ev@phystech.edu>
2021-07-15 23:21:47 +0300

    Начало разработки сайта-портфолио: добавлено 2 готовых сайта и начата главная страница

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.fairy-tail__slider').slick({
+        fade: true, //сделали слайдер более плавным
+        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
+        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
+        autoplay: true,
+    }); //сделали слайдер
+    
+    $('.our-trip__slider').slick({
+        fade: true, //сделали слайдер более плавным
+        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
+        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
+        autoplay: true,
+    }); //сделали слайдер
+});
