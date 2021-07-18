4a1c42de4d451f57a4e55572cba3c4b409ce6bf0
sharkova.ev <sharkova.ev@phystech.edu>
2021-07-18 12:40:35 +0300

    Добавлена форма отправки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(function(){
-    $('.fairy-tail__slider').slick({
-        fade: true, //сделали слайдер более плавным
-        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
-        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
-        autoplay: true,
-    }); //сделали слайдер
+    $(".footer__form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
     
-    $('.our-trip__slider').slick({
-        fade: true, //сделали слайдер более плавным
-        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
-        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
-        autoplay: true,
-    }); //сделали слайдер
 });
